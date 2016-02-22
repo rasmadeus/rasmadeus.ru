70285cc3386a031b26602d7ac98fd1d604623849
K. Kulikov <rasmadeus@gmail.com>
2016-02-23 01:24:47 +0300

    - added model ArticleGroup.

--- rapp/article/admin.py
+++ rapp/article/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from rapp.article import models
 
-# Register your models here.
+admin.site.register(models.ArticleGroup)
--- rapp/article/models.py
+++ rapp/article/models.py
@@ -2,4 +2,10 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class ArticleGroup(models.Model):
+    slug = models.CharField(unique=True, null=False, max_length=64, verbose_name="Url path")
+    header = models.CharField(null=False, max_length=64, verbose_name="Header")
+    description = models.TextField(null=False, verbose_name="Description")
+
+    def get_absolute_url(self):
+        return "/{slug}/".format(slug=self.slug)
