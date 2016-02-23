6bae8d0f56effb2f5029244c3ac524471d08d474
K. Kulikov <rasmadeus@gmail.com>
2016-02-24 01:46:30 +0300

    -fix url bug

--- rapp/article/urls.py
+++ rapp/article/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import url
-import views
+from rapp.article import views
 
 urlpatterns = [
     url(r'^(?P<slug>[-\w]+)/$', views.ArticleDetailView.as_view(), name='articles_group'),
