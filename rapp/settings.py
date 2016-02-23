e1132dff527356320399a3039ad7970394624c7a
K. Kulikov <rasmadeus@gmail.com>
2016-02-24 02:08:54 +0300

    -added south.

--- rapp/settings.py
+++ rapp/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rapp.article'
+    'rapp.article',
+    'south'
 )
 
 MIDDLEWARE_CLASSES = (
