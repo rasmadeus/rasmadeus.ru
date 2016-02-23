4638fd1b3ce537ff1bf5f11d3dabc0913c04f30c
K. Kulikov <rasmadeus@gmail.com>
2016-02-23 21:18:33 +0300

    - ouput articl_groups to template; - add migrations to ignore.

--- rapp/article/views.py
+++ rapp/article/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template.loader import get_template
+from rapp.article.models import ArticleGroup
 
 def index(request):
     template = get_template('index.html')
-    context = {'var': 'RAPP'}
+    context = {'article_groups': ArticleGroup.objects}
     return HttpResponse(template.render(context, request))
