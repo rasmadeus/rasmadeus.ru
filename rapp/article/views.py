0bd866c62e4879e6391295bb602ed94a840383e5
K. Kulikov <rasmadeus@gmail.com>
2016-02-27 19:03:33 +0300

    -add logout

--- rapp/article/views.py
+++ rapp/article/views.py
@@ -2,19 +2,21 @@ from django.http import HttpResponse
 from django.template.loader import get_template
 from rapp.article.models import Article
 from django.views.generic.detail import DetailView
-
+from django.contrib.auth import logout
 
 def _build_header(request):
     if request.user.is_authenticated():
         return {'username': request.user.username, 'url': "/logout", 'caption': "Logout"}
     else:
-        return {'username': "everyone", 'url': "/admin", 'caption': "Login"}
+        return {'username': "everyone", 'url': "/login", 'caption': "Login"}
 
 def index(request):
     template = get_template('index.html')
     context = {'articles': Article.objects.all(), 'header': _build_header(request)}
     return HttpResponse(template.render(context, request))
 
+def logout_view(request):
+    logout(request)
 
 class ArticleDetailView(DetailView):
     model = Article
--- rapp/urls.py
+++ rapp/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from rapp.article.views import index
+from rapp.article.views import index, logout_view
 
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', include(admin.site.urls)),
+    url(r'^logout/', logout_view),
     url(r'^articles/', include("rapp.article.urls")),
-    url(r'^$', index, name='index'),
+    url(r'^$', index),
 ]
