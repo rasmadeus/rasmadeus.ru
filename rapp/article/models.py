fdca8e5737e8387903820e43dbe9743ba211f470
K. Kulikov <rasmadeus@gmail.com>
2016-02-27 22:18:43 +0300

    fix migrations bug

--- rapp/article/models.py
+++ rapp/article/models.py
@@ -7,10 +7,10 @@ class Article(models.Model):
     slug = models.SlugField(unique=True, null=False, verbose_name="Url path")
     header = models.CharField(null=False, max_length=64, verbose_name="Header")
     content = models.TextField(null=False, verbose_name="Content")
-    author = models.ForeignKey(User, null=False, verbose_name="Author")
+    author = models.ForeignKey(User, unique=False, null=False, verbose_name="Author")
     edit_time = models.DateTimeField(default=datetime.now(), blank=True, verbose_name="Article edit time")
-    prev_article = models.ForeignKey('Article', related_name="article_prev", null=True, verbose_name="Previous article")
-    next_article = models.ForeignKey('Article', related_name="article_next", null=True, verbose_name="Next article")
+    prev_article = models.ForeignKey('Article', related_name="article_prev", unique=False, null=True, verbose_name="Previous article")
+    next_article = models.ForeignKey('Article', related_name="article_next", unique=False, null=True, verbose_name="Next article")
 
     def get_absolute_url(self):
         return "/{slug}/".format(slug=self.slug)
