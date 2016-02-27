fecc91edda909c1abb4b18d864d9ff4018a13c8d
K. Kulikov <rasmadeus@gmail.com>
2016-02-27 18:57:20 +0300

    -fix bug

--- rapp/article/views.py
+++ rapp/article/views.py
@@ -4,33 +4,15 @@ from rapp.article.models import Article
 from django.views.generic.detail import DetailView
 
 
-class _Header:
-    def __init__(self, request):
-        if request.user.is_authenticated():
-            self._username = request.user.username
-            self._url = "/logout"
-            self._caption = "Logout"
-        else:
-            self._username = "everyone"
-            self._url = "/admin"
-            self._caption = "Login"
-
-    @property
-    def username(self):
-        return self._username
-
-    @property
-    def url(self):
-        return self._url
-
-    @property
-    def caption(self):
-        return self._caption
-
+def _build_header(request):
+    if request.user.is_authenticated():
+        return {'username': request.user.username, 'url': "/logout", 'caption': "Logout"}
+    else:
+        return {'username': "everyone", 'url': "/admin", 'caption': "Login"}
 
 def index(request):
     template = get_template('index.html')
-    context = {'articles': Article.objects.all(), 'header': _Header(request)}
+    context = {'articles': Article.objects.all(), 'header': _build_header(request)}
     return HttpResponse(template.render(context, request))
 
 
@@ -40,5 +22,5 @@ class ArticleDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(ArticleDetailView, self).get_context_data(**kwargs)
-        context['username'] = _Header(self.request);
+        context['header'] = _build_header(self.request);
         return context
