1c0b017f08ab03e16fb31efe60dde358546fa221
K. Kulikov <rasmadeus@gmail.com>
2016-02-27 23:03:40 +0300

    add description to model

--- rapp/article/models.py
+++ rapp/article/models.py
@@ -3,9 +3,10 @@ from django.db import models
 from django.contrib.auth.models import User
 
 class Article(models.Model):
-    slug = models.SlugField(unique=True, null=False, verbose_name="Url path")
-    header = models.CharField(null=False, max_length=64, verbose_name="Header")
-    content = models.TextField(null=False, verbose_name="Content")
+    slug = models.SlugField(unique=True, null=False, default="Article slug", verbose_name="Url path")
+    header = models.CharField(null=False, max_length=64, default="Article header", verbose_name="Header")
+    description = models.TextField(null=False, default="Article description", verbose_name="Content")
+    content = models.TextField(null=False, default="Article conten", verbose_name="Content")
     author = models.ForeignKey(User, unique=False, null=False, verbose_name="Author")
     edit_time = models.DateTimeField(blank=False, verbose_name="Article edit time")
     prev_article = models.ForeignKey('Article', related_name="article_prev", unique=False, blank=True, null=True, verbose_name="Previous article")
