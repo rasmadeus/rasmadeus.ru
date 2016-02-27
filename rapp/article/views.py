efe57a38a63ab64e0384f9ead33081246aefa09f
K. Kulikov <rasmadeus@gmail.com>
2016-02-27 18:47:24 +0300

    -refactor index view

--- rapp/article/views.py
+++ rapp/article/views.py
@@ -3,10 +3,33 @@ from django.template.loader import get_template
 from rapp.article.models import Article
 from django.views.generic.detail import DetailView
 
+
+class _Header:
+    def __init__(self, request):
+        if request.user.is_authenticated():
+            self._username = request.user.username
+            self._url = "/logout"
+            self._caption = "Logout"
+        else:
+            self._username = "everyone"
+            self._url = "/admin"
+            self._caption = "Login"
+
+    def username(self):
+        return self._username
+
+    def url(self):
+        return self._url
+
+    def caption(self):
+        return self._caption
+
+
 def index(request):
     template = get_template('index.html')
-    context = {'articles': Article.objects.all()}
+    context = {'articles': Article.objects.all(), 'header': _Header(request)}
     return HttpResponse(template.render(context, request))
+
 
 class ArticleDetailView(DetailView):
     model = Article
@@ -14,4 +37,5 @@ class ArticleDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super(ArticleDetailView, self).get_context_data(**kwargs)
+        context['username'] = _Header(self.request);
         return context
