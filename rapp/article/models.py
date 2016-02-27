9dcdc87ed9842e39fdaa14dad5238b736344efb5
K. Kulikov <rasmadeus@gmail.com>
2016-02-27 23:50:50 +0300

    fix urls bugs

--- rapp/article/models.py
+++ rapp/article/models.py
@@ -4,10 +4,10 @@ from django.contrib.auth.models import User
 from django_markdown.models import MarkdownField
 
 class Article(models.Model):
-    slug = models.SlugField(unique=True, null=False, default="Article slug", verbose_name="Url path")
+    slug = models.SlugField(unique=True, null=False, default="article_slug", verbose_name="Url path")
     header = models.CharField(null=False, max_length=64, default="Article header", verbose_name="Header")
     description = models.TextField(null=False, default="Article description", verbose_name="Content")
-    content = MarkdownField(null=False, default="Article conten", verbose_name="Content")
+    content = MarkdownField(null=False, default="Article content", verbose_name="Content")
     author = models.ForeignKey(User, unique=False, null=False, verbose_name="Author")
     edit_time = models.DateTimeField(blank=False, verbose_name="Article edit time")
     prev_article = models.ForeignKey('Article', related_name="article_prev", unique=False, blank=True, null=True, verbose_name="Previous article")
