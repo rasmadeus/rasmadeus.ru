e0989d503053fc6f41a9e92deee9f2a22116e2eb
K. Kulikov <rasmadeus@gmail.com>
2016-02-27 17:24:17 +0300

    -fix url

--- rapp/urls.py
+++ rapp/urls.py
@@ -5,7 +5,7 @@ from rapp.article.views import index
 admin.autodiscover()
 
 urlpatterns = [
-    url(r'^articles/', include("rapp.article.urls")),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'$', index, name='index'),
+    url(r'^articles/', include("rapp.article.urls")),
+    url(r'^$', index, name='index'),
 ]
