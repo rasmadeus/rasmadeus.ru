308f6439ceeab03e2c582ea9e25b6a0c6e7387a9
K. Kulikov <rasmadeus@gmail.com>
2016-03-01 22:52:58 +0300

    Refactoring views

--- rapp/article/views.py
+++ rapp/article/views.py
@@ -4,41 +4,41 @@ from rapp.article.models import Article
 from django.views.generic.detail import DetailView
 from django.contrib.auth import logout
 
+
 def _get_greeting(request):
     return request.user.username if request.user.is_authenticated() else "everyone"
+
+
+def _get_default_common_data():
+    return {
+        'common_data': {
+            'title': 'K. Kulikov home page',
+            'keywords': 'c++, python, society life',
+            'description': 'K. Kulikov blog',
+            'author': 'K. Kulikov'
+        }
+    }
 
 def index(request):
     template = get_template('index.html')
     context = {
         'articles': Article.objects.all(),
         'greeting': _get_greeting(request),
-        'article': {
-            'common_data': {
-                'title': 'K. Kulikov home page',
-                'keywords': 'c++, python, army',
-                'description': '',
-                'author': 'K. Kulikov'
-            }
-        }
+        'article': _get_default_common_data()
     }
     return HttpResponse(template.render(context, request))
+
 
 def logout_view(request):
     logout(request)
     return index(request)
 
+
 def code_404_view(request):
     template = get_template('404.html')
     context = {
         'greeting': _get_greeting(request),
-        'article': {
-            'common_data': {
-                'title': '404 - page not found',
-                'keywords': '',
-                'description': '',
-                'author': ''
-            }
-        }
+        'article': _get_default_common_data()
     }
     return HttpResponse(template.render(context, request))
 
