bb07a4f9776a6cf1d4397797a948c9dc1312bd48
K. Kulikov <rasmadeus@gmail.com>
2016-03-03 02:24:04 +0300

    ckeditor

--- rapp/settings.py
+++ rapp/settings.py
@@ -140,6 +140,7 @@ STATICFILES_DIRS = (
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 CKEDITOR_JQUERY_URL = '//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js'
+CKEDITOR_UPLOAD_PATH = 'uploads/'
 CKEDITOR_CONFIGS = {
     'awesome_ckeditor': {
         'toolbar': 'Basic',
