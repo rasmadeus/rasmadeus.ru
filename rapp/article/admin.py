26a42f9c02e98c34151093fef148004483d2a84f
K. Kulikov <rasmadeus@gmail.com>
2016-03-03 02:00:29 +0300

    remove import

--- rapp/article/admin.py
+++ rapp/article/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from rapp.article import models
 from django import forms
-from redactor.widgets import RedactorEditor
+
 
 admin.site.register(models.Article)
 admin.site.register(models.CommonData)
